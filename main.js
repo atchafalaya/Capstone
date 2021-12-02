9e4a590d46f09f3c13bae7d8d7789345e8b95fd8
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-12-02 11:43:22 -0600

    Updated functions

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const tasksContainer = document.querySelector('#tasks-container')
 const form = document.querySelector('form')
-
 const baseURL = `http://localhost:4004/api/tasks`
 
 const tasksCallback = ({ data : tasks }) => {
    displayTaskers(tasks)
     
 }
+
+const removeTask = id => document.getElementById(`card-${id}`).remove()
+
 const errCallback = err => console.log(err)
 
-const getAllTaskers = () => axios.get(baseURL).then(tasksCallback).catch(errCallback)//((res) => console.log(res)) INSIDE the tasksCallback to test
+const getAllTaskers = () => axios.get(baseURL).then(tasksCallback).catch(errCallback)
 
-const createTask = body => axios.post(baseURL, body).then(tasksCallback).catch(errCallback)// post or get??
+const createTask = body => axios.post(baseURL, body).then(tasksCallback).catch(errCallback)//1 Input gets handed to Axios post
 
-const deleteTask = (id) => axios.delete(`${baseURL}/${id}`).then(tasksCallback).catch(errCallback)
+const deleteTask = (id) => axios.delete(`${baseURL}/${id}`).then(removeTask(id)).catch(errCallback)
 
-const checkTask = (id) => axios.put(`${baseURL}/${id}`).then(displayCrossOff).catch(errCallback)
+const checkTask = (id) => axios.put(`${baseURL}/${id}`).then(displayCrossOff(id)).catch(errCallback)
 
 function submitFormHandler(e) {
     e.preventDefault()
@@ -23,30 +25,27 @@ function submitFormHandler(e) {
     let task = document.querySelector('#enterTask')
 
     let bodyObj = {
-        task: task.value,
-        
+        task: task.value,  
     }
     createTask(bodyObj) 
     task.value = ''
-    
 }
 //creates a div for the task cards and adds tasks within
 function createTaskCard(task) {
     const taskCard = document.createElement('div')
     taskCard.classList.add('tasker-card')
+    taskCard.id = `card-${task.id}`
     
     taskCard.innerHTML = 
     `
-    <p class="task" id="task">${task.task}</p> 
+    <p class="task" id="task-${task.id}">${task.task}</p> 
 
     <button id="deleteBtn" onclick = "deleteTask('${task.id}')">X</button>
 
     <button id="checkBtn" onclick = "checkTask('${task.id}')">Done</button>
-
     `
     tasksContainer.appendChild(taskCard)
 }
-
 //loops through the database and creates task cards for each task
 function displayTaskers(arr) {
     tasksContainer.innerHTML = ``
@@ -54,12 +53,17 @@ function displayTaskers(arr) {
         createTaskCard(arr[i])
     }
 }
-function displayCrossOff() {
-    let cross = document.querySelector('.task')
-    cross.classList.add('checked')
-
+function displayCrossOff(id) {
+    let cross = document.getElementById(`task-${id}`)
+    //make array out of classlist and see if checked then toggle
+    if (Array.from(cross.classList).includes('checked')) {
+        cross.classList.remove('checked')
+    } else {
+        cross.classList.add('checked')
+    }
 }
- 
+
+
 getAllTaskers()//shows tasks
 
 form.addEventListener("submit",submitFormHandler)//event listener for task submit button
